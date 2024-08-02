12b15a17adafd2e514e350ddf3cf520ee55a3df1
Laura <lpadgett23@gmail.com>
2024-08-02 16:31:17 -0400

    docker compose file for postgres, volume and adminer; set up successful load to postgres container for 3Ws extraction; updated codes_list external file to include lists of the assessment data endpoints and the metadata endpoints

--- extract_3Ws_data.py
+++ extract_3Ws_data.py
@@ -3,6 +3,7 @@ import requests
 import json
 import logging
 import openpyxl
+from secrets_config import api_key
 
 def extract_3Ws_data(csv_path: str) -> list[dict]:
     # read list of country codes
